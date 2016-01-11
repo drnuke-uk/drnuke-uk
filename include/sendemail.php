e1a4ea89c0e3e784c971934bbbce42f919422291
Jamess <jamesg@tiscali.co.uk>
2016-01-11 14:16:55 +0000

    updates

---
+++ include/sendemail.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once('phpmailer/class.phpmailer.php');
+
+$mail = new PHPMailer();
+
+if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+    if( $_POST['template-contactform-name'] != '' AND $_POST['template-contactform-email'] != '' AND $_POST['template-contactform-message'] != '' ) {
+
+        $name = $_POST['template-contactform-name'];
+        $dob = $_POST['template-contactform-dob'];
+        $email = $_POST['template-contactform-email'];
+        $phone = $_POST['template-contactform-phone'];
+        $study = $_POST['template-contactform-study'];
+        $message = $_POST['template-contactform-message'];
+
+        //$subject = isset($subject) ? $subject : 'New Message From Contact Form';
+        $subject = 'New Message From Website Contact Form';
+
+        $botcheck = $_POST['template-contactform-botcheck'];
+
+        $toemail = 'james@drnuke.co.uk'; // Your Email Address
+        $toname = 'James Godbold'; // Your Name
+
+        if( $botcheck == '' ) {
+
+            $mail->SetFrom( $email , $name );
+            $mail->AddReplyTo( $email , $name );
+            $mail->AddAddress( $toemail , $toname );
+            $mail->Subject = $subject;
+
+            $name = isset($name) ? "Name: $name<br><br>" : '';
+            $dob = isset($dob) ? "Date of Birth: $dob<br><br>" : '';
+            $email = isset($email) ? "Email: $email<br><br>" : '';
+            $phone = isset($phone) ? "Phone: $phone<br><br>" : '';
+            $study = isset($study) ? "Study: $study<br><br>" : '';
+            $message = isset($message) ? "Message: $message<br><br>" : '';
+
+            $referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>This Form was submitted from: ' . $_SERVER['HTTP_REFERER'] : '';
+
+            $body = "$name $dob $email $phone $study $message $referrer";
+
+            $mail->MsgHTML( $body );
+            $sendEmail = $mail->Send();
+
+            if( $sendEmail == true ):
+                echo 'We have <strong>successfully</strong> received your Message and will get back to you as soon as possible.';
+            else:
+                echo 'Email <strong>could not</strong> be sent due to some Unexpected Error. Please Try Again later.<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '';
+            endif;
+        } else {
+            echo 'Bot <strong>Detected</strong>.! Clean yourself Botster.!';
+        }
+    } else {
+        echo 'Please <strong>Fill up</strong> all the Fields and Try Again.';
+    }
+} else {
+    echo 'An <strong>unexpected error</strong> occured. Please Try Again later.';
+}
+
+?>
